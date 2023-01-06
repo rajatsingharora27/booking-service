c240de443b31dfec570fb738134e1662ae7d21df
rajat <rajatsingharora27@gmail.com>
2023-01-06 14:57:17 +0530

    Added booking model

---
+++ src/utiles/errors/validation-error.js
@@ -0,0 +1,17 @@
+const { StatusCodes } = require("http-status-codes");
+
+class ValidationError extends Error {
+  constructor(error) {
+    let explanation = [];
+    error.errors.forEach((err) => {
+      explanation.push(err.message);
+    });
+    super();
+    this.name = "Validation Error";
+    this.message = "Not able to validate the data sent in the request";
+    this.explanation = explanation;
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
+module.exports = ValidationError;
